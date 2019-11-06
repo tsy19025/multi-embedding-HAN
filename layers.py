8790395c9e83e934d11e79abcd1b561a90c4f871
tsy19025 <tangsuyao@pku.edu.cn>
2019-11-06 18:47:31 +0800

    Add GraphAttentionNetwork

--- layers.py
+++ layers.py
@@ -1,35 +1,28 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from scipy import sparse
 
+# train weight-matrix W and attention function
 class GraphAttentionLayer(nn.Module):
-    def __init__(self, input_feature_size, output_feature_size, dropout, alpha, concat = True):
+    def __init__(self, input_features, outputs, alpha):
         super(GraphAttentionLayer, self).__init__()
-        self.dropout = dropout
-        self.input_feature_size = input_feature_size
-        self.out_feature_size = out_feature_size
-        self.alpha = alpha
-        self.concat = concat
 
-        self.W = nn.Parameter(torch.zeros(size = (input_feature_size, output_feature_size)))
-        nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        
-        self.a = nn.Parameter(torch.zeros(size=(output_feature_size * 2, 1)))
-        nn.init.xavier_uniform_(self.a.data, gain=1.414)
-        
+        self.input_features = input_features
+        self.outputs = self.outputs
+
+        self.W = nn.Parameter(torch.zero([input_features, outputs]))
+        self.a = nn.Parameter(torch.zero([2 * input_features, 1]))
         self.leakyrelu = nn.LeakyReLU(self.alpha)
-    def forward(self, input, adj):
-        hidden = torch.mm(input, self.W)
-        N = hidden.size()[0]
-        a_input = torch.cat([hidden.repeat(1, N).view(N * N, -1), hidden.repeat(N, 1)], dim=1).view(N, -1, 2 * self.output_feature_size)
-        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
-        
-        zero_vec = -9e15 * torch.ones_like(e)
-        attention = torch.where(adj > 0, e, zero_vec)
-        attention = F.softmax(attention, dim=1)
-        attention = F.dropout(attention, self.dropout, training=self.training)
-        h_prime = torch.matmul(attention, h)
-        
-        if self.concat: return F.elu(h_prime)
-        else: return h_prime
+    def forward(self, adj, input):
+        n = input.size()[0]
+        h = torch.mm(input, self.W)
+
+        a_input = torch.cat([h.repeat(1, n).view(n * n, -1)], dim = 1).view(n, -1, 2 * self.outputs)
+        e = self.leakyrelu(torch.matmul(a_input, self.a))
+
+        zeros_vec = -9e15 * torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zeros_vec)
+        attention = torch.softmax(attention, dim = 1)
+        h = torch.matmul(attention, h)
+        return h
+
--- models.py
+++ models.py
@@ -1,22 +1,19 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from layers import GraphAttentionLayer
 
-class GAT(nn.Module):
-    def __init__(self, feature_size, hidden_size, output_size, dropout, alpha, head_size):
-        super(GAT, self).__init__()
-        
-        self.dropout = dropout
-        self.attentions = [GraphAttentionLayer(feature_size, hidden_size, dropout, alpha) for i in range(head_size)]
+class GraphAttentionNetwork(nn.Module):
+    def __init__(self, input_features, hiddens, outputs, nheads, alpha):
+        super(GraphAttentionNetwork, self).__init__()
+        self.input_features = input_features
+        self.outputs = outputs
 
-        self.out_att = GraphAttentionLayer(hidden_size * head_size, output_size, dropout, alpha, False)
-    def forward(self, x, adj):
-        x = F.dropout(x, self.dropout, training = self.training)
-        x = torch.cat([att(x, adj) for att in self.attentions], dim = 1)
-        x = F.dropout(x, self.dropout, training = self.training)
-        x = F.elu(self.out_att(x, adj))
-        return F.log_softmax(x, dim = 1)
-
-class HeteGAT_multi(nn.Module):
-    def __init__(self, feature_size, hidden_size, output_size, ):
+        self.layer = nn.Linear(input_features, input_features)
+        self.attentions = [GraphAttentionLayer(input_features, hiddens, alpha) for i in range(nheads)]
+        self.attentionend = GraphAttentionLayer(hiddens * nheads, outputs, alpha)
+    def forward(self, input, adj):
+        x = self.layer(input)
+        x = torch.cat([attention(x, adj) for attention in self.attentions], dim = 1)
+        x = self.attentionend(x, adj)
+        x = torch.softmax(x, dim = 1)
+        return x
