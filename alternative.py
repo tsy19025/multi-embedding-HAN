754a9d4ca7fdcf0dfcc23740f140ab7942b7cc7e
tsy19025 <tangsuyao@pku.edu.cn>
2019-12-07 15:48:33 +0800

    Add mean pooling as an alternative to HomoAttention

--- alternative.py
+++ alternative.py
@@ -18,19 +18,17 @@ class LSTM(nn.Module):
         input = torch.cat((u.unsqueeze(1), v), dim = 1)
         input = input.permute(1, 0, 2)
         output, _ = self.layer(input)
-        return output[-1].view(batch_size, -1)
+        # output: [nlayers, batch_size, output_size]
+        return output[-1]
 
-class 
-        '''
-        w = torch.sum(v * u.view(batch_size, 1, n_facet, emb_dim), dim = 3)
-
-        x = v.permute(1, 0, 2, 3)
-        x = x.view(neighbor_size, batch_size, -1)
-        x = self.layer(x).view(batch_size, neighbor_size, self.n_facet, self.emb_dim)
-
-        u = torch.sum(x * w.view(batch_size, neighbor_size, self.n_facet, 1), dim = 1)
-        return u.view(batch_size, self.n_facet * self.emb_dim)
-        '''
+class mean_pooling(nn.Module):
+    def __init__(self):
+        super(mean_pooling, self).__init__()
+    def forward(self, node_feature, node_neighbor_feature):
+        n = node_neighbor_feature.shape[1]
+        x = torch.cat((node_feature.unsqueeze(1), node_neighbor_feature), dim = 1)
+        x = torch.sum(x, dim = 1).squeeze() / (n + 1)
+        return x
 
 if __name__ == "__main__":
     batch_size = 3
@@ -42,5 +40,5 @@ if __name__ == "__main__":
     node_feature = torch.rand(batch_size, n_facet * emb_dim)
     node_neighbor_feature = torch.rand(batch_size, neighbor_size, n_facet * emb_dim)
 
-    model = LSTM(n_facet, emb_dim, nlayers)
-    print(model(node_feature, node_neighbor_feature).shape)
+    model = mean_pooling()
+    print(model(node_feature, node_neighbor_feature))
