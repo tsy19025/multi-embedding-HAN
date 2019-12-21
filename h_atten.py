63078dfba6bb421ed9dacacad9a1456d7ee92f48
jamesyifan <yifanwang@pku.edu.cn>
2019-12-21 14:53:20 +0800

    smaller data

--- h_atten.py
+++ h_atten.py
@@ -10,15 +10,16 @@ class HomoAttention(nn.Module):
 
     def forward(self, features, neigh_features):
         batch_size = features.shape[0]
-        neighbor_size = neigh_features.shape[1]
-        x = features.view(batch_size, self.n_facet, self.emb_dim)
-        z = neigh_features.view(batch_size, neighbor_size, self.n_facet, self.emb_dim)
-        x = fn.normalize(x, dim=2)
-        z = fn.normalize(z, dim=3)
-        a = torch.sum(z*x.view(batch_size, 1, self.n_facet, self.emb_dim), dim=3)
-        a = fn.softmax(a, dim=1)
-        u = torch.sum(z*a.view(batch_size, neighbor_size, self.n_facet, 1), dim=1)
-        return u.view(batch_size, self.n_facet*self.emb_dim)
+        n_with_neg = features.shape[1]
+        neighbor_size = neigh_features.shape[2]
+        x = features.view(batch_size, n_with_neg, self.n_facet, self.emb_dim)
+        z = neigh_features.view(batch_size, n_with_neg, neighbor_size, self.n_facet, self.emb_dim)
+        x = fn.normalize(x, dim=3)
+        z = fn.normalize(z, dim=4)
+        a = torch.sum(z*x.view(batch_size, n_with_neg, 1, self.n_facet, self.emb_dim), dim=4)
+        a = fn.softmax(a, dim=2)
+        u = torch.sum(z*a.view(batch_size, n_with_neg, neighbor_size, self.n_facet, 1), dim=2)
+        return u.view(batch_size, n_with_neg, self.n_facet*self.emb_dim)
 
 class HeteAttention(nn.Module):
     def __init__(self, emb_dim, n_facet, niter):
@@ -30,19 +31,20 @@ class HeteAttention(nn.Module):
         # self.metapath_features = metapath_features
     def forward(self, features, metapath_featuers):
         batch_size = features.shape[0]
-        neighbor_size = metapath_featuers.shape[1]
-        x = features.view(batch_size, self.n_facet, self.emb_dim)
-        z = metapath_featuers.view(batch_size, neighbor_size, self.n_facet, self.emb_dim)
-        x = fn.normalize(x, dim=2)
-        z = fn.normalize(z, dim=3)
+        n_with_neg = features.shape[1]
+        neighbor_size = metapath_featuers.shape[2]
+        x = features.view(batch_size, n_with_neg, self.n_facet, self.emb_dim)
+        z = metapath_featuers.view(batch_size, n_with_neg, neighbor_size, self.n_facet, self.emb_dim)
+        x = fn.normalize(x, dim=3)
+        z = fn.normalize(z, dim=4)
         u = x
         for clus_iter in range(self.n_iter):
-            p = torch.sum(z*u.view(batch_size, 1, self.n_facet, self.emb_dim), dim=3)
-            p= fn.softmax(p, dim=2)
-            u = torch.sum(z*p.view(batch_size, neighbor_size, self.n_facet, 1), dim=1)
+            p = torch.sum(z*u.view(batch_size, n_with_neg, 1, self.n_facet, self.emb_dim), dim=4)
+            p= fn.softmax(p, dim=3)
+            u = torch.sum(z*p.view(batch_size, n_with_neg, neighbor_size, self.n_facet, 1), dim=2)
             u += x
             if clus_iter < self.n_iter - 1:
-                u = fn.normalize(u, dim=2)
+                u = fn.normalize(u, dim=3)
         return u.view(batch_size, self.n_facet*self.emb_dim)
 
 class UserItemAttention(nn.Module):
@@ -57,20 +59,21 @@ class UserItemAttention(nn.Module):
 
     def forward(self, user_emb, item_emb):
         batch_size = user_emb.shape[0]
+        n_with_neg = user_emb.shape[1]
         ###user self attention
-        u_emb = user_emb.view(batch_size, self.n_facet, self.emb_dim)
-        u_p = torch.matmul(u_emb, torch.transpose(u_emb, 1, 2))
-        u_p = fn.softmax(u_p, dim=2)
+        u_emb = user_emb.view(batch_size, n_with_neg, self.n_facet, self.emb_dim)
+        u_p = torch.matmul(u_emb, torch.transpose(u_emb, 2, 3))
+        u_p = fn.softmax(u_p, dim=3)
         u_emb_combined = torch.matmul(u_p, u_emb)
         ###item self attention
-        i_emb = item_emb.view(batch_size, self.n_facet, self.emb_dim)
-        i_p = torch.matmul(i_emb, torch.transpose(i_emb, 1, 2))
-        i_p = fn.softmax(i_p, dim=2)
+        i_emb = item_emb.view(batch_size, n_with_neg, self.n_facet, self.emb_dim)
+        i_p = torch.matmul(i_emb, torch.transpose(i_emb, 2, 3))
+        i_p = fn.softmax(i_p, dim=3)
         i_emb_combined = torch.matmul(i_p, i_emb)
         ###user item attention
-        u_i_p = torch.matmul(u_emb_combined, torch.transpose(i_emb_combined, 1, 2))
+        u_i_p = torch.matmul(u_emb_combined, torch.transpose(i_emb_combined, 2, 3))
         u_i_p = u_i_p.view(batch_size, self.n_facet*self.n_facet)
-        return torch.sum(u_i_p, 1)
+        return torch.sum(u_i_p, 2)
         # u_i_p = u_i_p.view(batch_size, self.n_facet*self.n_facet, 1)
         # u_i_p = fn.softmax(u_i_p, dim=1)
         # u_emb_combined = u_emb_combined.view(batch_size, self.n_facet, 1, self.emb_dim).expand(-1, -1, self.n_facet, -1)
