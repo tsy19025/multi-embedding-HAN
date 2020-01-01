15e2f5364a3482cfa92f78341d55cbd0dce50d99
tsy19025 <tangsuyao@pku.edu.cn>
2020-01-01 14:38:49 +0800

    MCRec

--- MCRec/main.py
+++ MCRec/main.py
@@ -20,9 +20,9 @@ def parse_args():
     parse = argparse.ArgumentParser(description="Run MCRec.")
     parse.add_argument('--dataset', default = 'yelp', help = 'Choose a dataset.')
     parse.add_argument('--epochs', type = int, default = 100)
-    parse.add_argument('--adjs_path', type = str, default = '/home1/wyf/Projects/gnn4rec/multi-embedding-HAN/yelp_dataset/adjs')
-    parse.add_argument('--data_path', type = str, default = '/home1/wyf/Projects/gnn4rec/multi-embedding-HAN/yelp_dataset')
-    parse.add_argument('--negetives', type = int, default = 50)
+    parse.add_argument('--adjs_path', type = str, default = '/home1/wyf/Projects/gnn4rec/multi-embedding-HAN/yelp_dataset/adjs/')
+    parse.add_argument('--data_path', type = str, default = '/home1/wyf/Projects/gnn4rec/multi-embedding-HAN/yelp_dataset/')
+    parse.add_argument('--negatives', type = int, default = 50)
     parse.add_argument('--batch_size', type = int, default = 60)
     parse.add_argument('--dim', type = int, default = 64)
     parse.add_argument('--sample', type = int, default = 50)
@@ -32,7 +32,8 @@ def parse_args():
     parse.add_argument('--decay', type = float, default = 0.98, help = 'learning rate decay rate')
     parse.add_argument('--feature_dim', type = int, default = 64)
     parse.add_argument('--save', type = str, default = 'model/')
-    parse.add_argument('--mode', type = str, default = 'train')
+    parse.add_argument('--K', type = int, default = 20)
+    parse.add_argument('--mode', type = str, default = 'test')
     # parse.add_argument()
 
     return parse.parse_args()
@@ -50,12 +51,12 @@ def train_one_epoch(model, train_data_loader, optimizer, loss_fn, epoch):
         user_input = torch.cat(user_input, 0).to(device)
         item_input = torch.cat(item_input, 0).to(device)
         label = torch.cat(label, 0).double().to(device)
-        # paths: paths * batch_size * negetives + 1 * path_num * timestamps
+        # paths: paths * batch_size * negatives + 1 * path_num * timestamps
         path_input = []
         for path in paths:
             batch_size, items_size, path_num, timestamps = path.shape
             path_input.append(path.view(batch_size * items_size, path_num, timestamps).to(device))
-        # path_input: paths * (batch_size * nege + 1) * path_num * timestamps
+        # path_input: paths * (batch_size * nega + 1) * path_num * timestamps
 
         output = model(user_input, item_input, path_input).squeeze(-1)
         loss = loss_fn(output.double(), label)
@@ -86,14 +87,20 @@ def eval(model, eval_data_loader, device, K):
             user_input = torch.cat(user_input, 0).to(device)
             item_input = torch.cat(item_input, 0).to(device)
             label = torch.cat(label, 0).double().to(device)
+            # print(user_input)
+            # print(item_input)
+            # print(pos, neg)
 
-            # paths: paths * batch_size * negetives + 1 * path_num * timestamps * length
+            # paths: paths * batch_size * negatives + 1 * path_num * timestamps * length
             path_input = []
             for path in paths:
                 batch_size, items_size, path_num, timestamps, length = path.shape
                 path_input.append(path.view(batch_size * items_size, path_num, timestamps, length).to(device))
-            # path_input: paths * (batch_size * nege + 1) * path_num * timestamps * length
+            # print(path_input)
+            # path_input: paths * (batch_size * nega + 1) * path_num * timestamps * length
             output = model(user_input, item_input, path_input).squeeze(-1)
+            # print("output")
+            # print(output)
             pred_items, indexs = torch.topk(output, K)
             gt_items = torch.nonzero(label)[:, 0].tolist()
             indexs = indexs.tolist()
@@ -104,6 +111,10 @@ def eval(model, eval_data_loader, device, K):
             p_at_k = getP(indexs, gt_items)
             r_at_k = getR(indexs, gt_items)
             ndcg_at_k = getNDCG(indexs, gt_items)
+            # print(indexs)
+            # print(gt_items)
+            # print(p_at_k, r_at_k, ndcg_at_k)
+            # print("--------------------------------------------------")
 
             eval_p.append(p_at_k)
             eval_r.append(r_at_k)
@@ -115,8 +126,8 @@ def eval(model, eval_data_loader, device, K):
     return mean_p, mean_r, mean_ndcg
 
 def valid(model, valid_data_loader, loss_fn):
-    mean_p, mean_r, mean_ndcg = eval(model, valid_data_loader, device, 20)
-    print('Valid:\tprecision@20:', mean_p, ', recall@20:', mean_r, ', ndcg@20', mean_ndcg)
+    mean_p, mean_r, mean_ndcg = eval(model, valid_data_loader, device, args.K)
+    print('Valid:\tprecision@', args.K, ':', mean_p, ', recall@', args.K, ':', mean_r, ', ndcg@', args.K, ':', mean_ndcg)
     return mean_p, mean_r, mean_ndcg
 
 if __name__ == '__main__':
@@ -125,35 +136,15 @@ if __name__ == '__main__':
     args = parse_args()
     if args.dataset == 'yelp':
         path_name = ['ub_path', 'uub_path', 'ubub_path', 'ubcab_path', 'ubcib_path']
-        if os.path.exists('path_data_all/' + path_name[0]):
-            paths = []
-            for name in path_name:
-                with open('path_data_all/' + name, 'rb') as f:
-                    paths.append(pickle.load(f))
-        else:
-            adjs_path = args.adjs_path
-            with open(adjs_path + '/adj_BCa', 'rb') as f:
-                adj_BCa = pickle.load(f)
-            with open(adjs_path + '/adj_BCi', 'rb') as f:
-                adj_BCi = pickle.load(f)
-            with open(adjs_path + '/adj_UB', 'rb') as f:
-                adj_UB = pickle.load(f)
-            with open(adjs_path + '/adj_UU', 'rb') as f:
-                adj_UU = pickle.load(f)
-
-            paths = utils.get_path(adj_BCa, adj_BCi, adj_UB, adj_UU, args.sample)
-            if not os.path.exists('path_data_all'): os.mkdir('path_data_all')
-            for i in range(5):
-                with open('path_data_all/' + path_name[i], 'wb') as f:
-                    pickle.dump(paths[i], f)
-        print("get path")
+        adjs_path = args.adjs_path
+        with open(adjs_path + 'adj_BCa', 'rb') as f: adj_BCa = pickle.load(f)
+        with open(adjs_path + 'adj_BCi', 'rb') as f: adj_BCi = pickle.load(f)
+        with open(adjs_path + 'adj_UB', 'rb') as f: adj_UB = pickle.load(f)
+        with open(adjs_path + 'adj_UU', 'rb') as f: adj_UU = pickle.load(f)
+        
         path_num = [1] + [args.sample] * 4
         timestamps = [2, 3, 4, 4, 4]
         path_type = [[0, 1], [0, 0, 1], [0, 1, 0, 1], [0, 1, 3, 1], [0, 1, 2, 1]]
-
-        with open(args.adjs_path + '/adj_UB', 'rb') as f:
-            adj_UB = pickle.load(f)
-        users, items = adj_UB.shape
 
         num_to_ids = []
         num_to_id_paths = []
@@ -166,29 +157,29 @@ if __name__ == '__main__':
         n_type = [len(num_to_id) for num_to_id in num_to_ids]
         
         if args.mode == 'train':
-            train_data_path = args.data_path + '/rates/rate_path'
+            train_data_path = args.data_path + 'rates/rate_train'
             with open(train_data_path, 'rb') as f:
                 train_data = pickle.load(f)
-            train_data_loader = DataLoader(dataset = YelpDataset(n_type, train_data, paths, path_num, timestamps, adj_UB, args.negetives, 'train'),
+            train_data_loader = DataLoader(dataset = YelpDataset(train_data, path_num, timestamps, adj_BCa, adj_BCi, adj_UB, adj_UU, args.negatives, args.sample, 'train'),
                                            batch_size = args.batch_size,
                                            shuffle = True,
                                            num_workers = 20,
                                            pin_memory = True)
 
-            valid_data_path = args.data_path + '/rates/valid_with_neg'
+            valid_data_path = args.data_path + 'rates/valid_with_neg'
             with open(valid_data_path, 'rb') as f:
                 valid_data = pickle.load(f)
-            valid_data_loader = DataLoader(dataset = YelpDataset(n_type, valid_data, paths, path_num, timestamps, adj_UB, 0, 'valid'),
+            valid_data_loader = DataLoader(dataset = YelpDataset(valid_data, path_num, timestamps,adj_BCa, adj_BCi, adj_UB, adj_UU, 0, args.sample, 'valid'),
                                            batch_size = 1,
                                            shuffle = True,
                                            num_workers = 20,
                                            pin_memory = True)
 
         print("read test data")
-        test_data_path = args.data_path + '/rates/test_with_neg'
+        test_data_path = args.data_path + 'rates/test_with_neg'
         with open(test_data_path, 'rb') as f:
             test_data = pickle.load(f)
-            test_data_loader = DataLoader(dataset = YelpDataset(n_type, test_data, paths, path_num, timestamps, adj_UB, 0, 'test'),
+            test_data_loader = DataLoader(dataset = YelpDataset(test_data, path_num, timestamps, adj_BCa, adj_BCi, adj_UB, adj_UU, 0, args.sample, 'test'),
                                           batch_size = 1, 
                                           shuffle = True,
                                           num_workers = 20,
--- MCRec/utils.py
+++ MCRec/utils.py
@@ -5,79 +5,6 @@ import random
 import sys
 import time
 
-'''
-def get_path(adj_BCa, adj_BCi, adj_UB, adj_UU):
-    adj_BU = adj_UB.T
-    adj_CaB = adj_BCa.T
-    adj_CiB = adj_BCi.T
-
-    ub_path = []
-    uub_path = []
-    ubub_path = []
-    ubcab_path = []
-    ubcib_path = []
-
-    users, items = adj_UB.shape
-    Cas = adj_BCa.shape[1]
-    Cis = adj_BCi.shape[1]
-    print("n_user: ", users)
-    print("n_item: ", items)
-    print("n_ca: ", Cas)
-    print("n_ci: ", Cis)
-    print("Generate paths")
-    max_path = 0
-    for i in range(users):
-        items = np.nonzero(adj_UB[i])[0]
-        ub = []
-        ubcab = []
-        ubcib = []
-        for item in items:
-            ub.append(str(i)+'-'+str(item))
-            cas = np.randome_np.nonzero(adj_BCa[item])[0]
-            print(cas)
-            cis = np.nonzero(adj_BCi[item])[0]
-            print(cis)
-            for ca in cas:
-                tmp_items = np.nonzero(adj_CaB[ca])[0]
-                print(len(tmp_items))
-                for tmp_item in tmp_items:
-                    ubcab.append(str(i) + '-' + str(item) + '-' + str(ca) + '-' + str(tmp_item))
-            for ci in cis:
-                tmp_items = np.nonzero(adj_CiB[ci])[0]
-                for tmp_item in tmp_items:
-                    ubcib.append(str(i) + '-' + str(item) + '-' + str(ci) + '-' + str(tmp_item))
-        ub_path.append(ub)
-        ubcab_path.append(ubcab)
-        ubcib_path.append(ubcib)
-        print(len(ubcab))
-        print(len(ubcib))
-        sys.exit(0)
-        max_path = max(max_path, len(ubcab))
-        max_path = max(max_path, len(ubcib))
-    print("Step1 end.")
-    print(max_path)
-    sys.exit(0)
-
-    for i in range(users):
-        ubub = []
-        for j in range(users):
-            for pathi in ub_path[i]:
-                for pathj in ub_path[j]:
-                    ubub.append(pathi+'-'+pathj)
-        ubub_path.append(ubub)
-    print("Step2 end.")
-
-    for i in range(users):
-        users = np.nonzero(adj_UU[i])[0]
-        uub = []
-        for j in users:
-            for pathj in ub_path[j]:
-                uub.append(str(i) + '-' + pathj)
-        uub_path.append(uub)
-    print("Step3 end.")
-    return ub_path, uub_path, ubub_path, ubcab_path, ubcib_path
-'''
-
 def get_path(adj_BCa, adj_BCi, adj_UB, adj_UU, sample, sample_a_size = 50, sample_b_size = 50):
     paths = [{}, {}, {}, {}, {}]
     adj_BU = adj_UB.T
@@ -96,9 +23,9 @@ def get_path(adj_BCa, adj_BCi, adj_UB, adj_UU, sample, sample_a_size = 50, sampl
         if len(bca) > sample_b_size: bca = np.random.choice(bca, size = sample_b_size, replace = False)
         if len(bci) > sample_b_size: bci = np.random.choice(bci, size = sample_b_size, replace = False)
 
-        item_bu.append(bu)
-        item_bca.append(bca)
-        item_bci.append(bci)
+        item_bu.append(set(bu))
+        item_bca.append(set(bca))
+        item_bci.append(set(bci))
 
     begin_ticks = time.time()
     for u in range(users):
@@ -133,12 +60,15 @@ def get_path(adj_BCa, adj_BCi, adj_UB, adj_UU, sample, sample_a_size = 50, sampl
                     if adj_UB[user][item] == 1: path.append([u, item, user, i])
             n = len(path)
             # print(n)
-            if n > sample: tmp = np.random.choice(range(n), size = sample, replace = False)
-            elif n > 0: tmp = np.random.choice(range(n), size = sample, replace = True)
+            if n > sample:
+                tmp = np.random.choice(range(n), size = sample, replace = False)
+                tmp = [path[t] for t in tmp]
+            elif n > 0:
+                tmp = np.random.choice(range(n), size = sample, replace = True)
+                tmp = [path[t] for t in tmp]
             else:
-                path = [[0, 0, 0, 0]] * sample
-                tmp = [0] * sample
-            paths[2][(u, i)] = np.array(list(path[t] for t in tmp))
+                tmp = [[0, 0, 0, 0]] * sample
+            paths[2][(u, i)] = np.array(list(tmp))
 
             # ubcab
             # print("ubcab")
@@ -174,20 +104,49 @@ def get_path(adj_BCa, adj_BCi, adj_UB, adj_UU, sample, sample_a_size = 50, sampl
     return paths
 
 class YelpDataset(Dataset):
-    def __init__(self, n_type, data, paths, path_num, timestamps, adj_UB, negetives, mode):
-        self.n_user = n_type[0]
-        self.n_item = n_type[1]
+    def __init__(self, data, path_num, timestamps, adj_BCa, adj_BCi, adj_UB, adj_UU, negatives, sample, mode):
+        self.n_user, self.n_item = adj_UB.shape
         self.data = data
         self.path_num = path_num
         self.timestamps = timestamps
-        self.n_negetive = negetives
+        self.n_negative = negatives
+        self.adj_BCa = adj_BCa
+        self.adj_BCi = adj_BCi
+        self.adj_UU = adj_UU
         self.adj_UB = adj_UB
+        self.sample = sample
         self.mode = mode
-        self.paths = paths
+        
+        adj_BU = adj_UB.T
+        self.item_bu = {}
+        self.item_bca = {}
+        self.item_bci = {}
 
-    def sample_negetive_item_for_user(self, user, negetive):
+        for i in range(self.n_item):
+            bu = np.nonzero(adj_BU[i])[0]
+            bca = np.nonzero(adj_BCa[i])[0]
+            bci = np.nonzero(adj_BCi[i])[0]
+
+            if len(bu) > 50: bu = np.random.choice(bu, size = 50, replace = False)
+            self.item_bu[i] = set(bu)
+            self.item_bca[i] = set(bca)
+            self.item_bci[i] = set(bci)
+
+        self.user_uu = {}
+        self.user_ub = {}
+        for u in range(self.n_user):
+            uu = np.nonzero(adj_UU[u])[0]
+            ub = np.nonzero(adj_UB[u])[0]
+
+            if len(uu) > 50: uu = np.random.choice(uu, size = 50, replace = False)
+            if len(ub) > 50: ub = np.random.choice(ub, size = 50, replace = False)
+            self.user_uu[u] = uu
+            self.user_ub[u] = ub
+
+
+    def sample_negative_item_for_user(self, user, negative):
         items = []
-        for t in range(negetive):
+        for t in range(negative):
             item = np.random.randint(0, self.n_item)
             while self.adj_UB[user][item] == 1:
                 item = np.random.randint(0, self.n_item)
@@ -195,21 +154,65 @@ class YelpDataset(Dataset):
         # print("user: ", user, item)
         return items
 
+    def get_path(self, u, i, idx):
+        path = []
+        if idx == 0:
+            if self.adj_UB[u][i] == 1: return [[u, i]]
+            return [[0, 0]]
+        if idx == 1:
+            path = [user for user in self.user_uu[u] if user in self.item_bu[i]]
+            n = len(path)
+            if n == 0: return np.zeros([self.sample, 3])
+            if n > self.sample: path = np.random.choice(path, size = self.sample, replace = False)
+            else: path = np.random.choice(path, size = self.sample)
+            return [[u, user, i] for user in path]
+        if idx == 2:
+            for item in self.user_ub[u]:
+                for user in self.item_bu[i]:
+                    if self.adj_UB[user][item] == 1:
+                        path.append([u, item, user, i])
+            n = len(path)
+            if n == 0: return np.zeros([self.sample, 4])
+            if n > self.sample: tmp = np.random.choice(range(n), size = self.sample, replace = False)
+            else: tmp = np.random.choice(range(n), size = self.sample)
+            return [path[t] for t in tmp]
+        if idx == 3:
+            for item in self.user_ub[u]:
+                for ca in self.item_bca[i]:
+                    if self.adj_BCa[item][ca] == 1:
+                        path.append([u, item, ca, i])
+            n = len(path)
+            if n == 0: return np.zeros([self.sample, 4])
+            if n > self.sample: tmp = np.random.choice(range(n), size = self.sample, replace = False)
+            else: tmp = np.random.choice(range(n), size = self.sample)
+            return [path[t] for t in tmp]
+        if idx == 4:
+            for item in self.user_ub[u]:
+                for ci in self.item_bci[i]:
+                    if self.adj_BCi[item][ci] == 1:
+                        path.append([u, item, ci, i])
+            n = len(path)
+            if n == 0: return np.zeros([self.sample, 4])
+            if n > self.sample: tmp = np.random.choice(range(n), size = self.sample, replace = False)
+            else: tmp = np.random.choice(range(n), size = self.sample)
+            return [path[t] for t in tmp]
     def __getitem__(self, index):
+        n_path = len(self.path_num)
         if self.mode == 'train':
             user = self.data[index]['user_id']
-            items = [self.data[index]['business_id']] + self.sample_negetive_item_for_user(user, self.n_negetive)
+            items = [self.data[index]['business_id']] + self.sample_negative_item_for_user(user, self.n_negative)
             
             path_inputs = []
-            for i in range(len(self.paths)):
+            for i in range(n_path):
                 path_input = []
                 for item in items:
                     feature_path = []
-                    for path in self.paths[i][(user, item)]:
+                    paths = self.get_path(user, item, i)
+                    for path in paths:
                         feature_path.append(list(val for val in path))
                     path_input.append(feature_path)
                 path_inputs.append(torch.tensor(path_input, dtype = torch.int64))
-            return [user] * (self.n_negetive + 1), items, [1.0] + [0.0] * self.n_negetive, path_inputs
+            return [user] * (self.n_negative + 1), items, [1.0] + [0.0] * self.n_negative, path_inputs
         else:
             user = self.data[index]['user_id']
             pos_n = len(self.data[index]['pos_business_id'])
@@ -217,11 +220,12 @@ class YelpDataset(Dataset):
             items = self.data[index]['pos_business_id'] + self.data[index]['neg_business_id']
 
             path_inputs = []
-            for i in range(len(self.paths)):
+            for i in range(n_path):
                 path_input = []
                 for item in items:
                     feature_path = []
-                    for path in self.paths[i][(user, item)]:
+                    paths = self.get_path(user, item, i)
+                    for path in paths:
                         feature_path.append(list([val] for val in path))
                     path_input.append(feature_path)
                 path_inputs.append(torch.tensor(path_input, dtype = torch.int64))
